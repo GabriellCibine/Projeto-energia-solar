df003632b2c830a40b9de6a8caa989cd28580b95
Gabriellcibine <gabriellcibine@gmail.com>
2025-05-22 16:25:21 -0300

    fim da estlização para telas a baixo de 900px

--- script.js
+++ script.js
@@ -20,3 +20,47 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+/**/
+
+const verMaisBtnNS = document.querySelector(".ver-mais-btn-ns");
+  const imagensExtrasNS = document.querySelectorAll(".extra-ns");
+
+  if (verMaisBtnNS) {
+    verMaisBtnNS.addEventListener("click", function () {
+      const mostrando = [...imagensExtrasNS].some(img => img.classList.contains("mostrar-ns"));
+
+      imagensExtrasNS.forEach(img => {
+        img.classList.toggle("mostrar-ns", !mostrando);
+      });
+
+      verMaisBtnNS.textContent = mostrando ? "Ver mais" : "Ver menos";
+
+      if (!mostrando) {
+        setTimeout(() => {
+          verMaisBtnNS.scrollIntoView({ behavior: "smooth", block: "end" });
+        }, 300);
+      }
+    });
+  }
+
+
+  /**/
+const btnISS = document.querySelector('.ver-mais-btn-iss');
+  const imgISS = document.querySelector('.img-iss2');
+
+  btnISS.addEventListener('click', () => {
+    imgISS.classList.toggle('mostrar-iss');
+    btnISS.textContent = imgISS.classList.contains('mostrar-iss') ? 'Ver menos' : 'Ver mais';
+  });
+
+
+  /**/
+
+  document.querySelector('.ver-mais-btn-ldm').addEventListener('click', function () {
+    document.querySelectorAll('.extra-ldm').forEach(function (img) {
+      img.classList.toggle('mostrar-ldm');
+    });
+
+    this.textContent = this.textContent === 'Ver mais' ? 'Ver menos' : 'Ver mais';
+  });
